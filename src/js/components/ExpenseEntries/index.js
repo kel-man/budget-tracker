3825f307a8af45e26cf438e9bd9c96c575bf3ddb
Kelen Y. Yafuso <kyyafuso@gmail.com>
2019-10-02 12:10:15 -0700

    Adds connect to indices, action creators, switch functions in reducers

--- src/js/components/ExpenseEntries/index.js
+++ src/js/components/ExpenseEntries/index.js
@@ -1,3 +1,14 @@
+import { connect } from 'react-redux';
 import ExpenseEntries from './ExpenseEntries';
 
-export default ExpenseEntries;
+function mapStoreToProps(store) {
+    return {
+        description: store.expense.description,
+        amount: store.expense.amount,
+        lineItems: store.expense.lineItems
+    };
+}
+
+
+// This might look odd but connect returns a function that is then called with the component itself.
+export default connect(mapStoreToProps)(ExpenseEntries);
